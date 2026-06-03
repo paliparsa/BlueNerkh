957654151589f8bb9c63914e8bd1f9c3861febe4
paliparsa <121433935+paliparsa@users.noreply.github.com>
2026-06-03 16:37:08 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // BlueNerkh API config
 // بعد از Deploy کردن Worker، لینک Worker را اینجا بگذار.
 // مثال: window.BLUENERKH_API_URL = "https://bluenerkh-api.yourname.workers.dev";
-window.BLUENERKH_API_URL = "https://YOUR-WORKER-URL.workers.dev";
+window.BLUENERKH_API_URL = "https://bluenerkh-api.iampaliparsa.workers.dev";
